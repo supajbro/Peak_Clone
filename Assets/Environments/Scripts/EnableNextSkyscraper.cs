d18ea5842f8067ce0d0f8840a16db3bf471e7013
supajbro <supajbro@gmail.com>
2025-07-08 22:58:44 +1000

    Syncing up enabled skyscrapers across all clients

--- Assets/Environments/Scripts/EnableNextSkyscraper.cs
+++ Assets/Environments/Scripts/EnableNextSkyscraper.cs
@@ -1,15 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class EnableNextSkyscraper : MonoBehaviour
+public class EnableNextSkyscraper : NetworkBehaviour
 {
     [SerializeField] private GameObject _next;
+    [SyncVar(hook = nameof(OnNextSkyscraperStateChanged))]
+    private bool _isNextActive = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (!isServer) return;
+
+        if (other.CompareTag("Player") && !_isNextActive)
         {
-            _next.SetActive(true);
+            _isNextActive = true;
         }
+    }
+
+    private void OnNextSkyscraperStateChanged(bool oldValue, bool newValue)
+    {
+        _next.SetActive(newValue);
+    }
+
+    public override void OnStartClient()
+    {
+        // Late joiners will have the SyncVar's current value
+        _next.SetActive(_isNextActive);
     }
 }
